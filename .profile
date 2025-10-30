ef2e5931bb373b47529130c53aa30dbe42b4ca0c
kicapman1x <ahhannan995@gmail.com>
2025-10-30 18:36:59 +0800

    ldap

---
+++ .profile
@@ -0,0 +1,8 @@
+##infra##
+export CERTS_DIR='/home/daddy/apps/lab_infra/ssl/certs'
+export LDAP_HOME='/home/daddy/apps/lab_infra/ldap/apacheds-2.0.0.AM28-SNAPSHOT'
+
+alias startldap='$LDAP_HOME/bin/apacheds.sh start'
+alias stopldap='$LDAP_HOME/bin/apacheds.sh stop'
+
+alias mldapsearch='ldapsearch -H ldap://localhost:10389 -D "uid=admin,ou=system" -w "$(grep admin_password $SECRETS_DIR/ldap_cred | cut -d"=" -f2)"'
---
+++ ldap_setup/ldap_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+wget https://dlcdn.apache.org//directory/apacheds/dist/2.0.0.AM27/apacheds-2.0.0.AM27.tar.gz
+tar zxfv apacheds-2.0.0.AM27.tar.gz
+wget https://dlcdn.apache.org/directory/studio/2.0.0.v20210717-M17/ApacheDirectoryStudio-2.0.0.v20210717-M17-linux.gtk.x86_64.tar.gz
+tar zxfv ApacheDirectoryStudio-2.0.0.v20210717-M17-linux.gtk.x86_64.tar.gz ##i personally dont use studio cause waste time zzz 
+
+#modifying partition
+base64 han.gg.txt
+#substitute the partition id contextentry
+
+startldap
+
+#test
+ldapsearch -H ldap://localhost:10389 -D "uid=admin,ou=system" -w "secret" -b "dc=han,dc=gg" "(objectClass=*)"
+
+#change pw
+ldapmodify -H ldap://localhost:10389 -D "uid=admin,ou=system" -w "secret" -f change_password.ldif
+
+#add user
+
+ldapadd -H ldap://localhost:10389 -D "uid=admin,ou=system" -w "secret" -f daddy.ldif 
